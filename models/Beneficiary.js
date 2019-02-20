88b8dbf8fc263598fbb434423f5345b5e96639f1
Optimist GM <optimist_gm@hotmail.com>
2019-02-20 15:15:37 +0400

    Beneficiary Edited

--- models/Beneficiary.js
+++ models/Beneficiary.js
@@ -1,84 +1,87 @@
 // import needed libraries
-const Sequelize = require('sequelize');
-const settings = require('../config/settings');
-const Category = require('./Category');
-const Wilayat = require('./Wilayat');
+const Sequelize = require("sequelize");
+const settings = require("../config/settings");
+const Category = require("./Category");
+const Wilayat = require("./Wilayat");
 
 // import database connection details
 const conn = settings.connection;
 
 // create connection
 const sequelize = new Sequelize(conn.database, conn.user, conn.password, {
-    host: conn.host,
-    dialect: conn.dialect,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    operatorsAliases: false
+  host: conn.host,
+  dialect: conn.dialect,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  operatorsAliases: false
 });
 
 // check database connection
-sequelize.authenticate().then(() => {
-    console.log('Connection has been established successfully.');
-}).catch(err => {
-    console.error('Unable to connect to the database:', err);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 // schema
-const Beneficiary = sequelize.define('beneficiaries', {
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        allowNull: false,
-        autoIncrement: true
-    },
-    file_id: {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-        unique: true
-    },
-    name: {
-        type: Sequelize.STRING,
-        unique: false,
-        allowNull: false
-    },
-    date_of_birth: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: '1900-01-01'
-    },
-    nationality: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        defaultValue: 'UNKNOWN'
-    },
-    mobile: {
-        type: Sequelize.INTEGER,
-        unique: true,
-        allowNull: true
-    },
-    phone: {
-        type: Sequelize.STRING,
-        allowNull: true
-    },
-    passport: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: true
-    },
-    civil_id: {
-        type: Sequelize.INTEGER,
-        unique: true,
-        allowNull: true
-    },
-    address: {
-        type: Sequelize.STRING,
-        unique: false,
-        allowNull: false
-    }
+const Beneficiary = sequelize.define("beneficiaries", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true
+  },
+  file_id: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+    unique: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    unique: false,
+    allowNull: false
+  },
+  date_of_birth: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    defaultValue: "1900-01-01"
+  },
+  nationality: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: "UNKNOWN"
+  },
+  mobile: {
+    type: Sequelize.INTEGER,
+    unique: true,
+    allowNull: true
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  passport: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: true
+  },
+  civil_id: {
+    type: Sequelize.INTEGER,
+    unique: true,
+    allowNull: true
+  },
+  address: {
+    type: Sequelize.STRING,
+    unique: false,
+    allowNull: false
+  }
 });
 
 // foreign key relationship
@@ -86,11 +89,14 @@ Beneficiary.belongsTo(Category);
 Beneficiary.belongsTo(Wilayat);
 
 // create table and sync database
-sequelize.sync().then(() => {
-    console.log('Beneficiaries Table Created!');
-}).catch((err) => {
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Beneficiaries Table Created!");
+  })
+  .catch(err => {
     console.log(err);
-});
+  });
 
 // export model globaly
 module.exports = Beneficiary;
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,13 @@
 // import needed libraries
-const express = require('express');
+const express = require("express");
 const Joi = require("joi");
 const encrypt = require("../config/encryption");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const settings = require("../config/settings");
-const mail = require('../config/sendemail');
-const Category = require('../models/Category');
+const mail = require("../config/sendemail");
+const Category = require("../models/Category");
+const Beneficiary = require("../models/Beneficiary");
 const router = express.Router();
 
 // get JWT secret key
@@ -17,210 +18,332 @@ const host = settings.HOST;
 
 // define login schema
 const loginSchema = Joi.object().keys({
-    id: Joi.string().required(),
-    password: Joi.string().min(6).required()
+  id: Joi.string().required(),
+  password: Joi.string()
+    .min(6)
+    .required()
 });
 
 // define reset password schema
 const resetPasswordSchema = Joi.object().keys({
-    id: Joi.string().required()
+  id: Joi.string().required()
 });
 
 // login route
 router.post("/login", (req, res) => {
-    // get user input
-    const data = req.body;
+  // get user input
+  const data = req.body;
 
-    // validate input
-    Joi.validate(data, loginSchema, (err, val) => {
-        if (err) { // validation fails
-            res.json({
-                status: "error",
-                message: "فشل في التعرف على البيانات المرسلة",
-                data: err.message
-            });
-        } else { // validation succeed
-            // check user through database
-            User.findOne({
-                where: {
-                    id: req.body.id
-                }
-            }).then(user => {
-                if (!user) {
-                    res.json({
-                        status: "error",
-                        message: "المستخدم غير مسجل في النظام"
-                    });
-                } else {
-                    // compare input password with db password
-                    encrypt.compareData(req.body.password, user.dataValues.password).then(val => {
-                        if (!val) {
-                            res.json({
-                                status: "error",
-                                message: "كلمة المرور غير صحيحة"
-                            });
-                        } else {
-                            // create a payload
-                            const { id, name, email, phone } = user;
-                            const payload = { id, name, email, phone };
-
-                            // generate a token
-                            jwt.sign({ payload }, jwtSecretKey, { expiresIn: "2 days" }, (err, token) => {
-                                if (err) {
-                                    res.status(403).json({
-                                        status: "error",
-                                        message: "لا يمكن تعيين رمز استخدام النظام",
-                                        data: err
-                                    });
-                                } else {
-                                    res.status(200).json({
-                                        status: "success",
-                                        message: "تم تسجيل الدخول بنجاح",
-                                        token: token,
-                                        data: payload
-                                    });
-                                }
-                            });
-                        }
-                    }).catch(err => {
-                        res.json({
-                            status: "error",
-                            message: "لا يمكن فك تشفير كلمة المرور",
-                            data: err
-                        });
-                    });
-                }
-            }).catch(err => {
-                res.json({
-                    status: "error",
-                    message: "لا يمكن الوصول لقاعدة البيانات",
-                    data: err
-                });
-            });
+  // validate input
+  Joi.validate(data, loginSchema, (err, val) => {
+    if (err) {
+      // validation fails
+      res.json({
+        status: "error",
+        message: "فشل في التعرف على البيانات المرسلة",
+        data: err.message
+      });
+    } else {
+      // validation succeed
+      // check user through database
+      User.findOne({
+        where: {
+          id: req.body.id
         }
-    });
+      })
+        .then(user => {
+          if (!user) {
+            res.json({
+              status: "error",
+              message: "المستخدم غير مسجل في النظام"
+            });
+          } else {
+            // compare input password with db password
+            encrypt
+              .compareData(req.body.password, user.dataValues.password)
+              .then(val => {
+                if (!val) {
+                  res.json({
+                    status: "error",
+                    message: "كلمة المرور غير صحيحة"
+                  });
+                } else {
+                  // create a payload
+                  const { id, name, email, phone } = user;
+                  const payload = { id, name, email, phone };
+
+                  // generate a token
+                  jwt.sign(
+                    { payload },
+                    jwtSecretKey,
+                    { expiresIn: "2 days" },
+                    (err, token) => {
+                      if (err) {
+                        res.status(403).json({
+                          status: "error",
+                          message: "لا يمكن تعيين رمز استخدام النظام",
+                          data: err
+                        });
+                      } else {
+                        res.status(200).json({
+                          status: "success",
+                          message: "تم تسجيل الدخول بنجاح",
+                          token: token,
+                          data: payload
+                        });
+                      }
+                    }
+                  );
+                }
+              })
+              .catch(err => {
+                res.json({
+                  status: "error",
+                  message: "لا يمكن فك تشفير كلمة المرور",
+                  data: err
+                });
+              });
+          }
+        })
+        .catch(err => {
+          res.json({
+            status: "error",
+            message: "لا يمكن الوصول لقاعدة البيانات",
+            data: err
+          });
+        });
+    }
+  });
 });
 
 // reset password route
-router.post('/reset-password', (req, res) => {
-    // get requested data
-    const data = req.body;
+router.post("/reset-password", (req, res) => {
+  // get requested data
+  const data = req.body;
 
-    // validate request
-    Joi.validate(data, resetPasswordSchema, (err, val) => {
-        if(err) {
-            console.log(err);
-            res.json({
-                status: "error",
-                message: "فشل في التعرف على البيانات المرسلة",
-                data: err.message
-            });
-        } else {
-            User.findOne({
-                where: {
-                    id: req.body.id
-                }
-            }).then(user => {
-                if(!user) {
-                    res.json({
-                        status: "error",
-                        message: "المستخدم غير مسجل في النظام"
-                    });
-                } else {
-                    // get payload
-                    const { id, name, email } = user;
-                    const payload = { id, name, email };
-
-                    // generate a new token
-                    jwt.sign({ payload }, jwtSecretKey, { expiresIn: "1h" }, (err, token) => {
-                        if(err) {
-                            res.status(403).json({
-                                status: "error",
-                                message: "لا يمكن تعيين رمز استخدام النظام",
-                                data: err
-                            });
-                        } else {
-                            // send an email
-                            mail(email, 'اعادة تعيين كلمة المرور', `<p>انقر الرابط التالي لاعادة تعيين كلمة المرور</p><br><a href='${host}/reset-password/${id}/${token}'>اضغط هنا</a>`).then(val => {
-                                if(val) {
-                                    res.json({
-                                        status: "success",
-                                        message: "تم ارسال ايميل لاعادة نعيين كلمة المرور"
-                                    });
-                                } else {
-                                    res.json({
-                                        status: "error",
-                                        message: "حدث خطا في الخادم .. لا يمكن ارسال ايميل لاعادة تعيين كلمة المرور"
-                                    });
-                                }
-                            }).catch(err => {
-                                res.json({
-                                    status: "error",
-                                    message: "حدث خطا في الخادم .. لا يمكن ارسال ايميل لاعادة تعيين كلمة المرور"
-                                });
-                            });
-                        }
-                    });
-                }
-            }).catch(err => {
-                res.json({
-                    status: "error",
-                    message: "لا يمكن الوصول لقاعدة البيانات",
-                    data: err
-                });
-            });
+  // validate request
+  Joi.validate(data, resetPasswordSchema, (err, val) => {
+    if (err) {
+      console.log(err);
+      res.json({
+        status: "error",
+        message: "فشل في التعرف على البيانات المرسلة",
+        data: err.message
+      });
+    } else {
+      User.findOne({
+        where: {
+          id: req.body.id
         }
-    });
+      })
+        .then(user => {
+          if (!user) {
+            res.json({
+              status: "error",
+              message: "المستخدم غير مسجل في النظام"
+            });
+          } else {
+            // get payload
+            const { id, name, email } = user;
+            const payload = { id, name, email };
+
+            // generate a new token
+            jwt.sign(
+              { payload },
+              jwtSecretKey,
+              { expiresIn: "1h" },
+              (err, token) => {
+                if (err) {
+                  res.status(403).json({
+                    status: "error",
+                    message: "لا يمكن تعيين رمز استخدام النظام",
+                    data: err
+                  });
+                } else {
+                  // send an email
+                  mail(
+                    email,
+                    "اعادة تعيين كلمة المرور",
+                    `<p>انقر الرابط التالي لاعادة تعيين كلمة المرور</p><br><a href='${host}/reset-password/${id}/${token}'>اضغط هنا</a>`
+                  )
+                    .then(val => {
+                      if (val) {
+                        res.json({
+                          status: "success",
+                          message: "تم ارسال ايميل لاعادة نعيين كلمة المرور"
+                        });
+                      } else {
+                        res.json({
+                          status: "error",
+                          message:
+                            "حدث خطا في الخادم .. لا يمكن ارسال ايميل لاعادة تعيين كلمة المرور"
+                        });
+                      }
+                    })
+                    .catch(err => {
+                      res.json({
+                        status: "error",
+                        message:
+                          "حدث خطا في الخادم .. لا يمكن ارسال ايميل لاعادة تعيين كلمة المرور"
+                      });
+                    });
+                }
+              }
+            );
+          }
+        })
+        .catch(err => {
+          res.json({
+            status: "error",
+            message: "لا يمكن الوصول لقاعدة البيانات",
+            data: err
+          });
+        });
+    }
+  });
 });
 
 // get categories data
-router.get('/categories', (req, res) => {
-    // load all avaliable categories
-    Category.findAll().then(val => {
-        res.json({
-            status: "success",
-            message: "تم جلب بيانات الحالات الاجتماعية",
-            data: val
-        });
-    }).catch(err => {
-        res.json({
-            status: "error",
-            message: "حدث خطا في قاعدة البيانات",
-            data: err
-        });
+router.get("/categories", (req, res) => {
+  // load all avaliable categories
+  Category.findAll()
+    .then(val => {
+      res.json({
+        status: "success",
+        message: "تم جلب بيانات الحالات الاجتماعية",
+        data: val
+      });
+    })
+    .catch(err => {
+      res.json({
+        status: "error",
+        message: "حدث خطا في قاعدة البيانات",
+        data: err
+      });
     });
+});
+
+// search for registered beneficiary
+router.post("/beneficiary", verifyToken, (req, res) => {
+  // verify token
+  jwt.verify(req.token, jwtSecretKey, (err, data) => {
+    if (err) {
+      res.json({
+        status: "error",
+        message: "رمز الوصول غير صحيح"
+      });
+    } else {
+      // get file number
+      const fileNumber = req.body.file_number;
+
+      // fetch beneficiary data
+      Beneficiary.findOne({ where: { file_id: fileNumber } })
+        .then(beneficiary => {
+          if (beneficiary) {
+            res.json({
+              status: "success",
+              message: "تم جلب بيانات المستفيد بنجاح",
+              data: beneficiary
+            });
+          } else {
+            res.json({
+              status: "error",
+              message: "لا توجد بيانات لهذا المستفيد"
+            });
+          }
+        })
+        .catch(err => {
+          res.json({
+            status: "error",
+            message: "حدث خطأ اثناء جلب بيانات المستفيد",
+            data: err
+          });
+        });
+    }
+  });
+});
+
+// add beneficiary
+router.post("/beneficiary/add", verifyToken, (req, res) => {
+  // verify token
+  jwt.verify(req.token, jwtSecretKey, (err, data) => {
+    if (err) {
+      res.json({
+        status: "error",
+        message: "رمز الوصول غير صحيح"
+      });
+    } else {
+      // beneficiary details
+      const {
+        file_number,
+        name,
+        dob,
+        nationality,
+        phone,
+        address,
+        category
+      } = req.body;
+
+      // add benefiaciary
+      Beneficiary.create({
+        file_id: file_number,
+        name,
+        date_of_birth: dob,
+        nationality,
+        mobile: phone,
+        address,
+        categoryId: category
+      })
+        .then(val => {
+          console.log(val);
+          res.json({
+            status: "success",
+            message: "تم ادخال بيانات المستفيد بنجاح",
+            data: val
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          res.json({
+            status: "error",
+            message: "حدث خطأ اثناء ادخال بيانات المستفيد",
+            data: err
+          });
+        });
+    }
+  });
 });
 
 // if route does not exist
 router.use((req, res, next) => {
-    res.status(404).json({
-        status: "error",
-        message: "لا يمكن الوصول الى الرابط المطلوب"
-    });
+  res.status(404).json({
+    status: "error",
+    message: "لا يمكن الوصول الى الرابط المطلوب"
+  });
 });
 
 // method to verify token
 function verifyToken(req, res, next) {
-    // get request header of type authorization
-    const bearerHeader = req.headers["authorization"];
+  // get request header of type authorization
+  const bearerHeader = req.headers["authorization"];
 
-    // check if authorization header is sent through the request
-    if (typeof bearerHeader !== "undefined") {
-        // extract token from the authorization header
-        const bearer = bearerHeader.split(" ");
-        const bearerToken = bearer[1];
+  // check if authorization header is sent through the request
+  if (typeof bearerHeader !== "undefined") {
+    // extract token from the authorization header
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
 
-        // set request token
-        req.token = bearerToken;
+    // set request token
+    req.token = bearerToken;
 
-        // exit middleware
-        next();
-    } else {
-        res.status(403).json({
-            status: "error",
-            message: "استخدام غير مصرح به"
-        });
-    }
+    // exit middleware
+    next();
+  } else {
+    res.status(403).json({
+      status: "error",
+      message: "استخدام غير مصرح به"
+    });
+  }
 }
 
 // export router
